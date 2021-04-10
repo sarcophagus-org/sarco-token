bf1b2f66a192ad21ba10bbd79c7e07173fe77289
Christopher Dancy <chrisotpherdancyblockchain@gmail.com>
2021-04-10 14:09:50 -0400

    tokenvestingv2 contract and test created

---
+++ contracts/TokenVestingv2.sol
@@ -0,0 +1,130 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.6.12;
+
+import "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
+import "@openzeppelin/contracts/math/SafeMath.sol";
+
+/**
+ * @title TokenVestingv2
+ * @dev A token holder contract that can release its token balance gradually like a
+ * typical vesting scheme, with a vesting period.
+ */
+
+contract TokenVestingv2 {
+    // The vesting schedule is time-based (i.e. using block timestamps as opposed to e.g. block numbers), and is
+    // therefore sensitive to timestamp manipulation (which is something miners can do, to a certain degree). Therefore,
+    // it is recommended to avoid using short time durations (less than a minute). Typical vesting schemes, with a
+    // duration of four years, are safe to use.
+
+    using SafeMath for uint256;
+    using SafeERC20 for IERC20;
+
+    event TokensReleased(IERC20 token, address beneficiary, uint256 amount);
+    event VestStarted(IERC20 token, address beneficiary, uint256 amount);
+
+    struct investment {
+        uint256 _totalTokens;
+        uint256 _releasedTokens;
+        uint256 _start;
+        uint256 _duration;
+        bool _investorAddressCreated;
+    }
+
+    //data mapping for token contract
+    mapping (IERC20 => mapping (address =>  investment)) public _tokenVest;
+
+    /**
+     * @dev Creates a vesting contract that vests its balance of any ERC20 token to the
+     * beneficiary, gradually in a linear fashion until start + duration. By then all
+     * of the balance will have vested.
+     * @param _beneficiary addresses of the beneficiaries to whom vested tokens are transferred
+     * @param _tokensToVest amounts of tokens for the beneficiaries
+     * @param _vestDuration duration in seconds of the period in which the tokens will vest
+     * @param _tokenAddress address of the token to be vested
+     */
+
+    function startVest(address _beneficiary, uint256 _tokensToVest, uint256 _vestDuration, IERC20 _tokenAddress) public {
+        require(_vestDuration > 0, 'TokenVestingv2: duration is 0');
+        require(getInvestorCreated(_tokenAddress, _beneficiary) != true, "_beneficiary already created for this token");
+        require(_beneficiary != address(0), "TokenVestingv2: beneficiary is the zero address");
+        require(_tokensToVest != 0, 'TokenVestingv2: amount is zero');
+        investment memory newInvestment = investment(_tokensToVest, 0, block.timestamp, _vestDuration, true);
+        _tokenVest[_tokenAddress][_beneficiary] = newInvestment;
+        
+        emit VestStarted(_tokenAddress, _beneficiary, _tokensToVest);
+    }
+
+     /**
+     * @return the start time of the token vesting.
+     */
+    function getStart(IERC20 token, address beneficiary) public view returns (uint256) {
+        return _tokenVest[token][beneficiary]._start;
+    }
+    
+    /**
+     * @return the total tokens of the token vesting.
+     */
+    function getTotalTokens(IERC20 token, address beneficiary) public view returns (uint256) {
+        return _tokenVest[token][beneficiary]._totalTokens;
+    }
+
+    /**
+     * @return the duration of the token vesting.
+     */
+    function getDuration(IERC20 token, address beneficiary) public view returns (uint256) {
+        return _tokenVest[token][beneficiary]._duration;
+    }
+
+    /**
+     * @return the duration of the token vesting.
+     */
+    function getInvestorCreated(IERC20 token, address beneficiary) public view returns (bool) {
+        return _tokenVest[token][beneficiary]._investorAddressCreated;
+    }
+
+
+    /**
+     * @return the amount of the token released.
+     */
+    function getReleasedTokens(IERC20 token, address beneficiary) public view returns (uint256) {
+        return _tokenVest[token][beneficiary]._releasedTokens;
+    }
+
+    /**
+     * @return the amount of tokens which can be claimed by a beneficiary.
+     */
+    function getReleasableAmount(IERC20 token, address beneficiary) public view returns (uint256) {
+        return _vestedAmount(token, beneficiary).sub(getReleasedTokens(token, beneficiary));
+    }
+
+    /**
+     * @notice Transfers vested tokens to beneficiary.
+     * @param beneficiary beneficiary to receive the funds
+     * @param token address of the token released
+     */
+    function release(IERC20 token, address beneficiary) public {
+        uint256 unreleased = getReleasableAmount(token, beneficiary);
+        require(unreleased > 0, "TokenVesting: no tokens are due");
+        _tokenVest[token][beneficiary]._releasedTokens = _tokenVest[token][beneficiary]._releasedTokens.add(unreleased);
+        token.safeTransfer(beneficiary, unreleased);
+        emit TokensReleased(token, beneficiary, unreleased);
+    }
+
+    /**
+     * @dev Calculates the amount that has already vested.
+     * @param beneficiary beneficiary address to check
+     * @param token address of the token vested
+     */
+    function _vestedAmount(IERC20 token, address beneficiary) private view returns (uint256) {
+        uint256 _startTime = getStart(token, beneficiary);
+        uint256 _durationTime = getDuration(token, beneficiary);
+        if (block.timestamp < _startTime) {
+            return 0;
+        } else if (block.timestamp >= _startTime.add(_durationTime)) {
+            return getTotalTokens(token, beneficiary);
+        } else {
+            return getTotalTokens(token, beneficiary).mul(block.timestamp.sub(_startTime)).div(_durationTime);
+        }
+    }
+}
---
+++ test/TokenVestingv2.test.js
@@ -0,0 +1,111 @@
+const { BN, constants, expectEvent, expectRevert, time } = require('@openzeppelin/test-helpers');
+const { ZERO_ADDRESS } = constants;
+
+const { expect } = require('chai');
+
+const ERC20Mintable = artifacts.require('Mintable');
+const TokenVestingv2 = artifacts.require('TokenVestingv2');
+
+contract('TokenVestingv2', accounts => {
+  const [ owner, beneficiary ] = accounts;
+
+  const amount = new BN('1000');
+
+  beforeEach(async function () {
+    this.vestingv2 = await TokenVestingv2.new({ from: owner })
+    this.token = await ERC20Mintable.new({ from: owner });
+    this.duration = time.duration.years(2);
+  });
+
+  it('reverts with a null beneficiary', async function () {
+    await expectRevert(
+      this.vestingv2.startVest(ZERO_ADDRESS, amount, this.duration, ZERO_ADDRESS, { from: owner }),
+      "TokenVestingv2: beneficiary is the zero address"
+    );
+  });
+
+  it('reverts with a null amount', async function () {
+    await expectRevert(
+      this.vestingv2.startVest(beneficiary, 0, this.duration, ZERO_ADDRESS, { from: owner }),
+      'TokenVestingv2: amount is zero'
+    );
+  });
+
+  it('reverts with a null duration', async function () {
+    await expectRevert(
+      this.vestingv2.startVest(beneficiary, amount, 0, ZERO_ADDRESS, { from: owner }),
+      'TokenVestingv2: duration is 0'
+    );
+  });
+
+  it('reverts with a duplicate investor', async function () {
+    await this.vestingv2.startVest(beneficiary, amount, this.duration, this.token.address, { from: owner })
+    await expectRevert(
+      this.vestingv2.startVest(beneficiary, amount, this.duration, this.token.address, { from: owner }),
+      "_beneficiary already created for this token"
+    );
+  });
+
+  context('once vest is started', function () {
+    beforeEach(async function () {
+      this.token = await ERC20Mintable.new({ from: owner });
+      this.vestingv2 = await TokenVestingv2.new({ from: owner });
+      this.duration = time.duration.years(2);
+
+      await this.token.mint(this.vestingv2.address, amount, { from: owner });
+
+      await this.vestingv2.startVest(beneficiary, amount, this.duration, this.token.address, { from: owner });
+      this.start = await this.vestingv2.getStart(this.token.address,beneficiary);
+      this.durationVest = await this.vestingv2.getDuration(this.token.address,beneficiary);
+    });
+
+    it('can get state', async function () {
+      expect(await this.vestingv2.getTotalTokens(this.token.address,beneficiary)).to.be.bignumber.that.equals(amount);
+      expect(await this.vestingv2.getDuration(this.token.address,beneficiary)).to.be.bignumber.that.equals(this.durationVest);
+      expect(await this.vestingv2.getInvestorCreated(this.token.address,beneficiary)).to.equal(true);
+    });
+
+    it('can be released', async function () {
+      await time.increaseTo(this.start.add(this.durationVest));
+      const { logs } = await this.vestingv2.release(this.token.address,beneficiary);
+      expectEvent.inLogs(logs, 'TokensReleased', {
+        token: this.token.address,
+        amount: await this.token.balanceOf(beneficiary),
+      });
+    });
+
+    it('should release proper amount', async function () {
+      await time.increaseTo(this.start.add(this.duration));
+
+      await this.vestingv2.release(this.token.address,beneficiary);
+      const releaseTime = await time.latest();
+
+      const releasedAmount = amount.mul(releaseTime.sub(this.start)).div(this.durationVest);
+      expect(await this.token.balanceOf(beneficiary)).to.be.bignumber.equal(releasedAmount);
+      expect(await this.vestingv2.getReleasedTokens(this.token.address,beneficiary)).to.be.bignumber.equal(releasedAmount);
+    });
+
+    it('should linearly release tokens during vesting period', async function () {
+      const vestingPeriod = this.durationVest;
+      const checkpoints = 4;
+
+      for (let i = 1; i <= checkpoints; i++) {
+        const now = this.start.add((vestingPeriod.muln(i).divn(checkpoints)));
+        await time.increaseTo(now);
+
+        await this.vestingv2.release(this.token.address,beneficiary);
+        const expectedVesting = amount.mul(now.sub(this.start)).div(this.durationVest);
+        expect(await this.token.balanceOf(beneficiary)).to.be.bignumber.equal(expectedVesting);
+        expect(await this.vestingv2.getReleasedTokens(this.token.address,beneficiary)).to.be.bignumber.equal(expectedVesting);
+      }
+    });
+
+    it('should have released all after end', async function () {
+      await time.increaseTo(this.start.add(this.durationVest));
+      await this.vestingv2.release(this.token.address,beneficiary);
+      expect(await this.token.balanceOf(beneficiary)).to.be.bignumber.equal(amount);
+      expect(await this.vestingv2.getReleasedTokens(this.token.address,beneficiary)).to.be.bignumber.equal(amount);
+    });
+  });
+});
+ 
